0859d1105d84904cb67bb06e8f718f8c133d19d4
Karim Jordan <karim@winter-rose.design>
2023-03-15 14:11:33 +0000

    fix error

--- src/routes/sse/+server.js
+++ src/routes/sse/+server.js
@@ -2,7 +2,7 @@ import sse from "$lib/sse.server.js";
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET() {
-  const id = crypto.randomUUID;
+  const id = crypto.randomUUID();
 
   const stream = sse.connect(id);
 
